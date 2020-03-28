3af7a37119f03dbc0e4f48bb8140365712e42ac4
andrefangeloni <andrefangeloni@gmail.com>
2020-03-28 11:53:44 -0300

    Added Profile and Session Controller

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const ONGController = require('./controllers/ONGController');
 const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
@@ -11,5 +13,9 @@ routes.post('/ongs', ONGController.store);
 routes.get('/incidents', IncidentController.index);
 routes.post('/incidents', IncidentController.store);
 routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/sessions', SessionController.store);
 
 module.exports = routes;
