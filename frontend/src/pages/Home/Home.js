2958e7b1a931a05ba9f14dc36b5dfed665d15187
andrefangeloni <andrefangeloni@gmail.com>
2020-03-30 00:31:34 -0300

    Get Incidents integrated

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 
 import './Home.css';
 
 const Home = () => {
+  const [incidents, setIncidents] = useState([]);
+
+  const ongId = localStorage.getItem('ongId');
+  const ongName = localStorage.getItem('ongName');
+
+  useEffect(() => {
+    loadProfiles();
+  }, []);
+
+  const loadProfiles = async () => {
+    const { data } = await api.get('/profile', {
+      headers: {
+        Authorization: ongId,
+      },
+    });
+    console.log(data);
+    setIncidents(data);
+  };
+
   return (
     <div className="home-container">
       <header>
         <img src={logo} alt="Be The Hero" />
-        <span>Bem vinda, APAD</span>
+        <span>Bem vinda, {ongName}</span>
         <Link className="button" to="/incidents/new">
           Cadastrar novo caso
         </Link>
@@ -21,66 +42,25 @@ const Home = () => {
       </header>
 
       <h1>Casos cadastrados</h1>
-      
+
       <ul>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
+        {incidents.map(incident => (
+          <li key={incident.id}>
+            <strong>CASO:</strong>
+            <p>{incident.title}</p>
 
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
+            <strong>DESCRIÇÃO:</strong>
+            <p>{incident.description}</p>
 
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
+            <strong>VALOR:</strong>
+            <p>{incident.value}</p>
 
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
-
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
-
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
-        <li>
-          <strong>CASO:</strong>
-          <p>Caso teste</p>
-
-          <strong>DESCRIÇÃO:</strong>
-          <p>Descrição teste</p>
-
-          <strong>VALOR:</strong>
-          <p>R$ 120,00</p>
-
-          <button type="button">
-            <FiTrash2 size={20} color="#a8a8b3" />
-          </button>
-        </li>
+            <button type="button">
+              <FiTrash2 size={20} color="#a8a8b3" />
+            </button>
+          </li>
+        ))}
       </ul>
-
     </div>
   );
 };
--- frontend/src/pages/Login/Login.js
+++ frontend/src/pages/Login/Login.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 import heroes from '../../assets/heroes.png';
@@ -8,15 +10,40 @@ import heroes from '../../assets/heroes.png';
 import './Login.css';
 
 const Login = () => {
+  const [id, setId] = useState('');
+
+  const history = useHistory();
+
+  const handleLogin = async e => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('/sessions', { id });
+
+      localStorage.setItem('ongId', id);
+      localStorage.setItem('ongName', response.data.name);
+
+      history.push('/home');
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
   return (
     <div className="login-container">
       <section className="form">
         <img src={logo} alt="Be The Hero" />
 
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Faça seu login</h1>
-          <input placeholder="Sua ID" />
-          <button type="submit" className="button">Entrar</button>
+          <input
+            value={id}
+            onChange={e => setId(e.target.value)}
+            placeholder="Sua ID"
+          />
+          <button type="submit" className="button">
+            Entrar
+          </button>
 
           <Link className="back-link" to="/register">
             <FiLogIn size={16} color="#e02041" />
@@ -27,7 +54,7 @@ const Login = () => {
 
       <img src={heroes} alt="Heroes" />
     </div>
-  )
+  );
 };
 
 export default Login;
