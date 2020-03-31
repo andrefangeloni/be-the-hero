eb3b9794f10935a7b1c32627a59420846559eb66
andrefangeloni <andrefangeloni@gmail.com>
2020-03-31 09:39:38 -0300

    Mobile inicial setup

---
+++ mobile/src/screens/Details/Details.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import styles from './styles';
+
+const Details = () => {
+  return <Text>Details</Text>;
+};
+
+export default Details;
---
+++ mobile/src/screens/Incidents/Incidents.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+// import styles from './styles';
+
+const Incidents = () => {
+  return <Text>Incidents</Text>;
+};
+
+export default Incidents;
