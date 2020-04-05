b31b743c69a2b96900c29ec83522a17f6bc6d397
andrefangeloni <andrefangeloni@gmail.com>
2020-04-05 00:00:16 -0300

    Styled Incidents done

--- mobile/src/screens/Incidents/Incidents.js
+++ mobile/src/screens/Incidents/Incidents.js
@@ -1,11 +1,26 @@
 import React from 'react';
-import { SafeAreaView, View, Text, Image } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  FlatList,
+  SafeAreaView,
+  TouchableOpacity,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/Feather';
 
 import logo from '../../assets/logo.png';
 
 import styles from './styles';
 
 const Incidents = () => {
+  const navigation = useNavigation();
+
+  const navigateToDetails = () => {
+    navigation.navigate('Details');
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -19,6 +34,32 @@ const Incidents = () => {
       <Text style={styles.description}>
         Escolhe um dos casos abaixo e salve o dia.
       </Text>
+
+      <FlatList
+        data={[1, 2, 3]}
+        style={styles.incidentList}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={incident => incident.toString()}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>ONG:</Text>
+            <Text style={styles.incidentValue}>APAD</Text>
+
+            <Text style={styles.incidentProperty}>CASO:</Text>
+            <Text style={styles.incidentValue}>Cadelinha atropelada</Text>
+
+            <Text style={styles.incidentProperty}>VALOR:</Text>
+            <Text style={styles.incidentValue}>R$ 120,00</Text>
+
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={() => navigateToDetails()}>
+              <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
+              <Icon name="arrow-right" size={16} color="#e02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
     </SafeAreaView>
   );
 };
--- mobile/src/screens/Incidents/styles.js
+++ mobile/src/screens/Incidents/styles.js
@@ -30,6 +30,36 @@ const styles = StyleSheet.create({
     lineHeight: 24,
     color: '#737380',
   },
+  incidentList: {
+    marginTop: 32,
+  },
+  incident: {
+    padding: 24,
+    marginBottom: 16,
+    borderRadius: 8,
+    backgroundColor: '#fff',
+  },
+  incidentProperty: {
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#41414d',
+  },
+  incidentValue: {
+    marginTop: 8,
+    marginBottom: 15,
+    fontSize: 15,
+    color: '#737380',
+  },
+  detailsButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  detailsButtonText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: '#e02041',
+  },
 });
 
 export default styles;
