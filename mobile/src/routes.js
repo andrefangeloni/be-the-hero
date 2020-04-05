78a7cf77e5b7bc13e37cc1be7360fa0d65708096
andrefangeloni <andrefangeloni@gmail.com>
2020-04-04 22:46:19 -0300

    Setup Routes

---
+++ mobile/src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const AppStack = createStackNavigator();
+
+import Incidents from './screens/Incidents/Incidents';
+import Details from './screens/Details/Details';
+
+const Routes = () => {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="Incidents" component={Incidents} />
+        <AppStack.Screen name="Details" component={Details} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Routes;
